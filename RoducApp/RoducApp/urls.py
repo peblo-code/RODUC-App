54f3ce427f623b014777fa94ddf4d45e4ee53f4d
Alexgdm <alexgustavodiesel@gmail.com>
2022-05-13 21:07:15 -0400

    Configuracion Entorno y Primera Migracion

--- RoducApp/RoducApp/urls.py
+++ RoducApp/RoducApp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('RoducWeb', include('RoducWeb.urls'))
 ]
