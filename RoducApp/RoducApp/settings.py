8e80c7b91526b28291ec6e0eccd8a5a3cfd7d55d
Alexgdm <alexgustavodiesel@gmail.com>
2022-05-13 20:46:00 -0400

    Configuracion BD

--- RoducApp/RoducApp/settings.py
+++ RoducApp/RoducApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'RoducApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'roducdb',
+        'USER' : 'root',
+        'PASSWORD': 'olimpiacampeon02',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Asuncion'
 
 USE_I18N = True
 
