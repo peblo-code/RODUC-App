84fe913d491ab1010d0ccf0529e9ae1024a13198
Pablo Benedix <pabling.benedix@gmail.com>
2022-05-15 21:33:31 -0400

    Detalle de vista de usuarios

--- RoducApp/restapi/serializers.py
+++ RoducApp/restapi/serializers.py
@@ -17,3 +17,20 @@ class UsuarioListSerializer(serializers.ModelSerializer):
             'modif_usuario',
             'modif_fecha',
         ]
+
+class UsuarioDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = [
+            'cod_usuario', 
+            'nombre_usuario',
+            'contraseña',
+            'nombres_del_usuario',
+            'apellidos_del_usuario', 
+            'direccion_email',
+            'estado', 
+            'alta_usuario', 
+            'alta_fecha', 
+            'modif_usuario',
+            'modif_fecha',
+        ]
--- RoducApp/restapi/urls.py
+++ RoducApp/restapi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('', views.UsuarioListAPIView.as_view(), name="usuario_list"), 
+    path('lista_usuarios', views.UsuarioListAPIView.as_view(), name="usuario_list"),
+    path('lista_usuarios/<str:nombre_usuario>', views.PizzeriaRetrieveAPIView.as_view(), name="usuario_detail"),  
 ]
