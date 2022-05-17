40d14bfcdd4035f4783e11f8ac886ea90da87f76
Pablo Benedix <pabling.benedix@gmail.com>
2022-05-17 10:53:28 -0400

    manage.py test

--- RoducApp/manage.py
+++ RoducApp/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Runn administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RoducApp.settings')
     try:
         from django.core.management import execute_from_command_line
