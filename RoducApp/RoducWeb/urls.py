ac42bcee27ea4204a81ec46c6dd80e7dfc7f7c74
Alexgdm <alexgustavodiesel@gmail.com>
2022-05-21 23:55:06 -0400

    Codigo mejor formateado

--- RoducApp/RoducWeb/urls.py
+++ RoducApp/RoducWeb/urls.py
@@ -4,46 +4,46 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.login, name = 'login'),
+    path('', views.login, name='login'),
     path('cerrar_sesion', views.cerrar_sesion, name='cerrar_sesion'),
-    path('inicio', views.inicio, name = 'inicio'),
+    path('inicio', views.inicio, name='inicio'),
 
-    #USUARIOS
+    # USUARIOS
     path('usuario', views.usuario, name='usuario'),
     path('detalle_usuario', views.detalle_usuario, name='detalle_usuario'),
     path('agregar_usuario', views.agregar_usuario, name='agregar_usuario'),
     path('actualizar_usuario', views.actualizar_usuario, name='actualizar_usuario'),
     path('eliminar_usuario', views.eliminar_usuario, name='eliminar_usuario'),
 
-    #PERFIL
+    # PERFIL
     path('perfil', views.perfil, name='perfil'),
     path('asignar_rol', views.asignar_rol, name='asignar_rol'),
     #path('detalleAsignaturasCarrera', views.detalleAsignaturasCarrera, name='detalleAsignaturasCarrera')
-    
-    #FACULTAD
+
+    # FACULTAD
     path('facultad', views.facultad, name='facultad'),
     path('agregar_facultad', views.agregar_facultad, name='agregar_facultad'),
     path('detalle_facultad', views.detalle_facultad, name='detalle_facultad'),
     path('actualizar_facultad', views.actualizar_facultad, name='actualizar_facultad'),
     path('eliminar_facultad', views.eliminar_facultad, name='eliminar_facultad'),
 
-    #CARRERA
+    # CARRERA
     path('carrera', views.carrera, name='carrera'),
 
-    #PLAN DE ESTUDIOS
+    # PLAN DE ESTUDIOS
     path('plan_estudio', views.plan_estudio, name='plan_estudio'),
     path('agregar_plan', views.agregar_plan, name='agregar_plan'),
     path('detalle_plan', views.detalle_plan, name='detalle_plan'),
     path('actualizar_plan', views.actualizar_plan, name='actualizar_plan'),
     path('eliminar_plan', views.eliminar_plan, name='eliminar_plan'),
-    
-    #SEMESTRES
+
+    # SEMESTRES
     path('semestre', views.semestre, name='semestre'),
     path('agregar_semestre', views.agregar_semestre, name='agregar_semestre'),
     path('detalle_semestre', views.detalle_semestre, name='detalle_semestre'),
     path('actualizar_semestre', views.actualizar_semestre, name='actualizar_semestre'),
     path('eliminar_semestre', views.eliminar_semestre, name='eliminar_semestre'),
 
-    #ASIGNATURAS
+    # ASIGNATURAS
     path('asignatura', views.asignatura, name='asignatura')
 ]
